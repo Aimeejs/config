3ca781ea278e836f17ab754c18922563618bd0de
gavinning <ningyubo@gmail.com>
2016-01-05 16:40:10 +0800

    1.重构核心代码，加入类概念，默认返回实例，兼容之前版本 2.支持set不存在的节点对象，程序自动生成空节点 3.更新说明文档

--- config.js
+++ config.js
@@ -4,101 +4,147 @@
  * Homepage https://github.com/Aimeejs/config
  */
 
-var is = require('is');
-var extend = require('extend');
-var __config = {};
+var is, extend, Class, Config, config;
 
-
-/**
- * 设置数据模型
- * @param   {String || Object} obj 模块id或数据模型对象
- */
-exports.init = function(obj){
-    // 当做模块路径处理
-    if(is.string(obj)){
-        __config = require(obj)
+try{
+    // For aimee
+    is = require('is');
+    extend = require('extend');
+    Class = require('class');
+}catch(e){
+    try{
+        // For node
+        is = require('aimee-is');
+        extend = require('aimee-extend');
+        Class = require('aimee-class');
     }
-
-    // 当做配置文件处理
-    if(is.plainObject(obj)){
-        __config = obj;
-    }
+    catch(e){}
 }
 
-/**
- * 单项配置设置，覆盖模式，推荐只用于单项配置
- * @param   {String} key   属性
- * @param   {[type]} value 属性值
- * @example config.set('dir.install', 'packages');
- */
-exports.set = function(key, value){
+Config = Class.create();
+config = new Config;
+config.Config = Config;
+module.exports = config;
 
-    // 获取配置文件原始数据
-    if(key === 'config'){
+/**
+ * 递归创建不存在子节点为空对象
+ * @param   {Object}  target   目标对象
+ * @param   {String}  key      要操作的key
+ * @param   {anyType} value    target.key
+ * @return  {Object}           target
+ * @example this.create({}, 'app.path', __dirname)
+ */
+Config.createObject = function(target, key, value){
+    var pop;
+    var data = target;
+    var arr = key.split('.');
+
+    do{
+        pop = arr.shift();
+        data[pop] = data[pop] || {};
+        data = data[pop];
+    }
+    while(arr.length > 1)
+
+    // 检查是否存在value
+    if(value){
+        // 节点创建完成尝试赋值
+        try{
+            eval('target.' + key + ' = value')
+        // 赋值失败则抛错
+        }catch(e){
+            throw e;
+        }
+    }
+
+    return target;
+}
+
+Config.include({
+    /**
+     * 设置数据模型
+     * @param   {String || Object} target 模块id或数据模型对象
+     */
+    init: function(target){
+        this.__config = {};
 
         // 当做模块路径处理
-        if(is.string(value)){
-            __config = require(value)
+        if(is.string(target)){
+            try{
+                this.__config = require(target)
+            }
+            catch(e){
+                throw new Error(target + ' is not a module id.')
+            }
         }
 
         // 当做配置文件处理
-        if(is.plainObject(value)){
-            __config = value;
+        if(is.plainObject(target)){
+            this.__config = target;
+        }
+    },
+
+    /**
+     * 单项配置设置，覆盖模式，推荐只用于单项配置
+     * @param   {String}  key   属性
+     * @param   {Anytype} value 属性值
+     * @example config.set('dir.install', 'packages');
+     */
+    set: function(key, value){
+        try{
+            eval('this.__config.' + key + ' = value')
+        }catch(e){
+            this.__config = Config.createObject(this.__config, key, value)
+        }
+    },
+
+    /**
+     * 获取配置
+     * @param   {String}  key 属性
+     * @return  {Anytype}     属性值
+     * @example config.get('dir.install'); // => packages
+     */
+    get: function(key){
+        if(!key){
+            return this.__config;
         }
 
-        return this;
-    }
+        try{
+            return eval('this.__config.' + key)
+        }catch(e){
+            throw e;
+        }
+    },
 
-    try{
-        eval('__config.' + key + ' = value')
-    }catch(e){}
-}
+    /**
+     * 多项配置设置，合并模式，推荐使用多项配置
+     * @param   {String}   key   合并的属性节点
+     * @param   {Anytype}   value 合并的对象map
+     * @example config.merge({dir: {install: 'packages'}});
+     * @example config.merge('dir', {install: 'packages'});
+     * @example config.merge('dir.install', 'packages');
+     */
+    merge: function(key, value){
+        var tmp;
 
-/**
- * 获取配置
- * @param   {String} key 属性
- * @return  {[type]}     属性值
- * @example config.get('dir.install'); // => packages
- */
-exports.get = function(key){
-    if(!key){
-        return __config;
-    }
+        if(!value){
+            value = key;
+            key = null;
+        }
 
-    try{
-        return eval('__config.' + key)
-    }catch(e){}
-}
+        // 检查key是否存在
+        if(key && typeof key !== 'string'){
+            key = null;
+        }
 
-/**
- * 多项配置设置，合并模式，推荐使用多项配置
- * @param   {[String]} key 合并的属性节点
- * @param   {Object}   obj 合并的对象map
- * @example config.merge({dir: {install: 'packages'}});
- * @example config.merge('dir', {install: 'packages'});
- * @example config.merge('dir.install', 'packages');
- */
-exports.merge = function(key, obj){
-    var tmp = {};
-
-    // 检查key是否存在
-    if(typeof key !== 'string'){
-        obj = key;
-        key = null;
-    }
-
-    try{
-        // 扩展到指定的key
+        // 合并到指定节点
         if(key){
-            extend(true, tmp, __config);
-            eval('tmp.' + key + ' = obj');
-            extend(true, __config, tmp);
+            tmp = Config.createObject({}, key, value);
+            extend(true, this.__config, tmp);
         }
-
-        // 扩展配置本身
-        else {
-            extend(true, __config, obj);
+        // 合并到根节点
+        else{
+            extend(true, this.__config, value);
         }
-
-    }catch(e){}
-}
+    }
+})
--- readme.md
+++ readme.md
@@ -1,17 +1,35 @@
-# config
+# Config
 
-Aimee框架的config模块，用于处理配置文件
+Aimee框架的config模块，用于管理配置文件
 
 
 #### Install
+
 ```
+// For Aimee
 aimee i config
 ```
 
+```
+// For Node
+npm i vpm-config
+```
+
+
 #### Example
 ```javascript
+// For Aimee
 var config = require('config');
 
+```
+```javascript
+// For Node
+var config = require('vpm-config');
+
+```
+
+
+```javascript
 config.init();
 
 // eg1
@@ -25,16 +43,51 @@ config.get('foo.bar');				// => 123
 
 // eg3
 config.merge('foo.bar', {
-	test: 123
+    test: 123
 });
 config.get('foo.bar');				// => {test: 123}
 ```  
 
+#### Instances
+```javascript
+var Config = config.Config;
+var config1 = new Config;
+
+config1.set('app.name', 'gavinning')
+console.log(config1.get('app')) // => {name: gavinning}
+```
 
 #### Api
 
+``config.init(target)``
+* ``@param`` ``target`` ``type: Object`` 可选，传入已有的配置
+* ``@param`` ``target`` ``type: String`` 可选，传入初试配置文件路径或模块id   
+```javascript
+config.init({a: 1})
 ```
-config.get 		// 获取配置
-config.set 		// 覆盖设置  
-config.merge 	// 合并设置  
+---
+
+``config.get(key)``
+* ``@param`` ``key`` ``type: String`` 可选，不传key默认返回完整的配置对象
+```javascript
+config.get() 	   // => {a: 1}
+config.get('a') 	// => 1
+```
+---
+
+``config.set(key, value)``
+* ``@des`` 覆盖配置文件中指定的key
+* ``@param`` ``key`` ``type: String`` 必选，需要更新的key
+* ``@param`` ``value`` ``type: AnyType`` 必选，任意数据类型均可
+```javascript
+config.set('app.name', 'gavinning') // => {app: {name: gavinning}}
+```
+---
+
+``config.merge(key, value)``
+* ``@des`` 合并到配置文件指定的key
+* ``@param`` ``key`` ``type: String`` 可选，没有key的时候直接更新到根对象(慎用)
+* ``@param`` ``value`` ``type: AnyType`` 必选，任意数据类型均可
+```javascript
+config.merge('app.path', '/app/gavinning') // => {app: {name: gavinning, path: '/app/gavinning'}}
 ```
